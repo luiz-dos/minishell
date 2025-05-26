1c43625dee4b8ab55abf2c95b00f3dd5ea1df8de
Guilherme Antonio Freire De Andrade <gufreire@c2r4s11.42lisboa.com>
2025-05-26 19:24:41 +0100

    fe

--- src/main/main.c
+++ src/main/main.c
@@ -56,8 +56,10 @@ int main(int ac, char **av, char **envp)
 	(void)av;
 	global_sig = 0;
 	if (!envp)
-		;
-		//min_env();
+	{
+		//shell()->envvar = min_env(envp);
+		shell()->envvar_export = create_lst_export();
+	}
 	else
 	{
 		shell()->envvar = create_lst_envvar(envp);
@@ -76,5 +78,6 @@ int main(int ac, char **av, char **envp)
  * TODO (expand_envvar): Alocar "expanded" aos poucos, de acordo com o tamanho das expansoes para evitar overflow
  * TODO: quotes ainda nao esta 100% indentico ao bash (add whitespace no input \2 \3 )
  * TODO: criar funcoes para lidar com escapes \
- * 
+ * cd sem nada diz que tem demasiados argumentos mas move para o HOME na mesma
+ * nao da para entrar no minishell quando ja dentro do minishell
 */
